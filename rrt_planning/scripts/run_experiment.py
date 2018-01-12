0d178ab8290f695f44a043c693e41b7e119e29b4
rebark <caro.beretta@gmail.com>
2018-01-12 17:39:42 +0100

    experiment node, part II

--- rrt_planning/scripts/run_experiment.py
+++ rrt_planning/scripts/run_experiment.py
@@ -1,6 +1,34 @@
 import os
+import sys
+import gflags
 
+from joblib import Parallel, delayed
+
+gflags.DEFINE_integer('n_jobs', -1, 'number of parallel experiments')
+#gflags.DEFINE_string('deadline', '60', 'deadline (in seconds)')
+gflags.DEFINE_integer('n_exp', 1, 'number of experiments')
+gflags.DEFINE_string('env_name', 'map', 'environment name')
+
+maps = ['map']
+algorithms = ['nh', 'rrt', 'rrt_star', 'theta_star_rrt', 'voronoi_rrt']
 
 if __name__ == '__main__':
-    print 'Running experiments'
-    os.system('roslaunch rrt_planning experiment.launch')
+    argv = gflags.FLAGS(sys.argv)
+    with open(filename) as f:
+        configurations = f.read().splitlines()
+
+    os.system('roscore')
+    print 'launching map server'
+    os.system('rosrun map_server map_server $(find rrt_planning)/maps/'
+               + env_name + '.yaml' )
+    print 'launching static tf'
+    os.system('rosrun tf static_transform_publisher 1 0 0 0 0 0 1 base_link map 100')
+
+    Parallel(n_jobs=gflags.FLAGS.n_jobs)(delayed(os.system)
+                                        ('rosrun rrt_planning experiment ' + os.getcwd() +
+                                         ' ' + alg
+                                         ' ' + env_name
+                                         ' ' + conf
+                                         ' ' + conf.index(value))
+                                         for alg in algorithms
+                                         for conf in confingurations
--- rrt_planning/src/Experiment.cpp
+++ rrt_planning/src/Experiment.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <chrono>
 
 #include <tf/transform_listener.h>
 #include <nav_core/base_global_planner.h>
@@ -8,21 +10,24 @@
 
 #include <rrt_planning/NHPlanner.h>
 
-rrt_planning::NHPlanner* getPlanner(const std::string& name, costmap_2d::Costmap2DROS* costmap_ros);
-void init(geometry_msgs::PoseStamped& start_pose,
+nav_core::BaseGlobalPlanner* getPlanner(const std::string& name, costmap_2d::Costmap2DROS* costmap_ros);
+void parse(const std::string& conf, geometry_msgs::PoseStamped& start_pose,
 				geometry_msgs::PoseStamped& goal_pose);
+void save(const std::string& filename, const std::string& conf, double t, std::vector<geometry_msgs::PoseStamped>& plan)
 
 
 int main(int argc, char** argv)
 {
 	ROS_FATAL_STREAM("Starting");
-	std::string planner_name, map;
-	ros::init(argc, argv, "experiment");
-	ros::NodeHandle private_nh("~/");
+	std::string planner_name, map, node_name, conf, row;
+	planner_name = argv[0];
+	map = argv[1];
+	conf = argv[2];
+	row = argv[3];
+	node_name = planner_name + map + row;
 
-	private_nh.param("planner_name", planner_name, std::string("NHPlanner"));
-	private_nh.param("map", map, std::string("map"));
-	ROS_FATAL_STREAM("map name: " + map);
+	ros::init(argc, argv, node_name);
+	ros::NodeHandle private_nh("~/");
 
 	//Costmap inizialization magics
     tf::TransformListener tf_(ros::Duration(10));
@@ -31,21 +36,28 @@ int main(int argc, char** argv)
 
 	ROS_FATAL_STREAM("Costmap loaded");
 
-	//Open file and start reading
+	//Convert start and goal
 	geometry_msgs::PoseStamped start_pose, goal_pose;
 	std::vector<geometry_msgs::PoseStamped> plan;
 
-	init(start_pose, goal_pose);
-	//Launch planner
-	rrt_planning::NHPlanner* planner = getPlanner(planner_name, costmap_ros);
-	ROS_FATAL_STREAM("Started planner: " + planner_name);
-	bool result = planner->makePlan(start_pose, goal_pose, plan);
-	ROS_FATAL_STREAM("Plan found: " + result);
+	parse(conf, start_pose, goal_pose);
 
+	//Launch planner
+	nav_core::BaseGlobalPlanner* planner = getPlanner(planner_name, costmap_ros);
+	ROS_FATAL_STREAM("Started planner: " + planner_name);
+
+	std::chrono::steady_clock::time::point t0 = steady::clock().now();
+	bool result = planner->makePlan(start_pose, goal_pose, plan);
+	std::chrono::duration<double> ex_time = steady::clock().now() - t0;
+	save(node_name, conf, t, ex_time.count());
+	private_nh.deleteParam("");
+
+	ROS_FATAL_STREAM("Plan found: " + result);
+	ros::Duration(5).sleep();
 	return 0;
 }
 
-void init(geometry_msgs::PoseStamped& start_pose, geometry_msgs::PoseStamped& goal_pose)
+void parse(const std::string& conf, geometry_msgs::PoseStamped& start_pose, geometry_msgs::PoseStamped& goal_pose)
 {
 	start_pose.pose.position.x = 24.6607;
 	start_pose.pose.position.y = 2.52053;
@@ -63,11 +75,33 @@ void init(geometry_msgs::PoseStamped& start_pose, geometry_msgs::PoseStamped& go
 	goal_pose.pose.orientation.z = 0.974315;
 	goal_pose.pose.orientation.w = 0.225188;
 
+}
 
+void save(const std::string& filename, const std::string& conf, double t, std::vector<geometry_msgs::PoseStamped>& plan)
+{
+	std::ofstream f;
+	f.open(filename + std::string(".exp"));
+
+	std::string d = std::string("_");
+
+	//Write configuration
+	f << conf << "\n";
+
+	//Write execution Time
+	f << t << "\n";
+
+	//Write path points
+	for(auto p : plan)
+	{
+		f << p.pose.position.x << d << p.pose.position.y << d
+		  << p.pose.orientation.z << d << p.pose.orientation.w << "\n";
+	}
+
+	f.close();
 }
 
 
-rrt_planning::NHPlanner* getPlanner(const std::string& name, costmap_2d::Costmap2DROS* costmap_ros)
+nav_core::BaseGlobalPlanner* getPlanner(const std::string& name, costmap_2d::Costmap2DROS* costmap_ros)
 {
 	if(name == "NHPlanner")
 	{
