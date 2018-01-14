9b7cad72125be78f16488da8faf813174d7f9c55
rebark <caro.beretta@gmail.com>
2018-01-14 14:42:52 +0100

    experiment node, part IV

--- rrt_planning/scripts/run_experiment.py
+++ rrt_planning/scripts/run_experiment.py
@@ -10,10 +10,30 @@ gflags.DEFINE_integer('n_jobs', 1, 'number of parallel experiments')
 gflags.DEFINE_string('deadline', '300', 'deadline (in seconds)')
 gflags.DEFINE_integer('n_exp', 1, 'number of experiments')
 gflags.DEFINE_string('env_name', 'map', 'environment name')
+gflags.DEFINE_string('model', 'differentialDrive', 'kinematic model')
 
 maps = ['map']
 #algorithms = ['nh', 'rrt', 'rrt_star', 'theta_star_rrt', 'voronoi_rrt']
 algorithms = ['nh']
+
+def experiment(a, c, i):
+    print ''
+    print '*************************************************'
+    print a, c, i
+    ns = a + '_' + 'map' + '_' + i
+    os.system('rosparam load config/' + a + '.yaml ' + ns)
+    os.system('rosparam load config/' + gflags.FLAGS.model + '.yaml ' + ns)
+    os.system('rosparam load config/' + 'global_costmap_params.yaml ' + ns)
+    os.system('rosparam load config/' + 'costmap_common_params.yaml ' + ns + '/global_costmap')
+    os.system('rosrun rrt_planning experiment ' +
+              ' ' + a +
+              ' ' + gflags.FLAGS.env_name +
+              ' ' + c +
+              ' ' + i +
+              ' ' + gflags.FLAGS.deadline +
+              ' ' + os.getcwd() + '/logs/')
+    print 'node has died'
+
 
 if __name__ == '__main__':
     argv = gflags.FLAGS(sys.argv)
@@ -21,8 +41,8 @@ if __name__ == '__main__':
     with open(filename) as f:
         configurations = f.read().splitlines()
     print configurations
-    #roscore = subprocess.Popen('roscore')
-    #time.sleep(1)
+    roscore = subprocess.Popen('roscore')
+    time.sleep(1)
     origWD = os.getcwd()
     print 'launching map server'
     map_dir = os.getcwd() + "maps/map.yaml"
@@ -31,19 +51,14 @@ if __name__ == '__main__':
     print 'launching static tf'
     tf = subprocess.Popen(['rosrun', 'tf', 'static_transform_publisher',
                             '1','0','0','0','0','0','1','base_link','map','100'])
-    #os.system('rosparam load config/nh.launch nh_map_1')
+    time.sleep(1)
 
-    Parallel(n_jobs=gflags.FLAGS.n_jobs)(delayed(os.system)
-                                        ('rosrun rrt_planning experiment ' +
-                                         ' ' + alg +
-                                         ' ' + gflags.FLAGS.env_name +
-                                         ' ' + conf +
-                                         ' ' + str(configurations.index(conf)) +
-                                         ' ' + gflags.FLAGS.deadline +
-                                         ' ' + os.getcwd() + '/logs/')
-                                         for alg in algorithms
-                                         for conf in configurations
+    Parallel(n_jobs=gflags.FLAGS.n_jobs)(delayed(experiment)
+                                        (alg, conf, str(configurations.index(conf)))
+                                        for alg in algorithms
+                                        for conf in configurations
                                         )
-    #subprocess.Popen.kill(roscore)
+    print 'magic is real'
     subprocess.Popen.kill(map_server)
     subprocess.Popen.kill(tf)
+    subprocess.Popen.kill(roscore)
